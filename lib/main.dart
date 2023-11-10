7c521b6bb7797018986ac3a2c7190504d03a6e92
Kabir Deula <kabirdeula167@gmail.com>
2023-11-10 16:09:17 +0545

    added individual file for function fetchProduct

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 // ignore_for_file: avoid_print
 
-import 'dart:io';
-import 'dart:convert';
-
+import 'package:ecommerce/controllers/controller.product.dart';
 import 'package:ecommerce/models/model.product.dart';
 import 'package:ecommerce/screens/screen.home.dart';
 import 'package:flutter/material.dart';
-
-import 'config/config.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -18,41 +14,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Future<List<Product>> products = fetchProducts();
+    Future<List<Product>> products = fetchProduct();
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.white,
-            elevation: 4.0,
-          ),
-          useMaterial3: true),
-      home: HomeScreen(products: products, title: 'Daraz Lite'),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 4.0,
+        ),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(
+        products: products,
+        title: 'Daraz Lite',
+      ),
     );
-  }
-
-  Future<List<Product>> fetchProducts() async {
-    const String apiUrl = "$baseUrl/api/products";
-    try {
-      await Future.delayed(const Duration(seconds: 2));
-
-      final HttpClientRequest request =
-          await HttpClient().getUrl(Uri.parse(apiUrl));
-      final HttpClientResponse response = await request.close();
-
-      if (response.statusCode == 200) {
-        final List<dynamic> jsonData =
-            json.decode(await response.transform(utf8.decoder).join());
-        final List<Product> products =
-            jsonData.map((data) => Product.fromMap(data)).toList();
-        return products;
-      } else {
-        throw Exception(
-            'Failed to fetch products. Status Code: ${response.statusCode}');
-      }
-    } catch (error) {
-      rethrow;
-    }
   }
 }
