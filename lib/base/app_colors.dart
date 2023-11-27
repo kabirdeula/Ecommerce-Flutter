79844fc08f6d1e84ab1c4496159089baca9e3861
Kabir Deula <kabirdeula167@gmail.com>
2023-11-27 06:57:07 +0545

    added colors

--- lib/base/app_colors.dart
+++ lib/base/app_colors.dart
@@ -1,48 +1,52 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color lPrimaryColor = const Color(0xFF24859C);
-  static Color dPrimaryColor = const Color(0xFF24859C);
+  static Color lPrimaryColor = const Color(0xFFFF7518);
+  static Color lSecondaryColor = const Color(0xFF878D91);
+  static Color lWhiteColor = const Color(0xFFFBFCF8);
 
-  static Color lBackgroundColor = const Color(0xFFF5F5F5);
-  static Color dBackgroundColor = const Color(0xFFF5F5F5);
+  // static Color lPrimaryColor = const Color(0xFF24859C);
+  // static Color dPrimaryColor = const Color(0xFF24859C);
 
-  static Color lDangerColor = const Color(0xFF760000);
-  static Color dDangerColor = const Color(0xFF760000);
+  // static Color lBackgroundColor = const Color(0xFFF5F5F5);
+  // static Color dBackgroundColor = const Color(0xFFF5F5F5);
 
-  static Color lBGColor = const Color(0xFFEBF4F1);
-  static Color dBGColor = const Color(0xFFEBF4F1);
+  // static Color lDangerColor = const Color(0xFF760000);
+  // static Color dDangerColor = const Color(0xFF760000);
 
-  static Color lDisableColor = const Color(0xFFD1D4D5);
-  static Color dDisableColor = const Color(0xFFD1D4D5);
+  // static Color lBGColor = const Color(0xFFEBF4F1);
+  // static Color dBGColor = const Color(0xFFEBF4F1);
 
-  static Color lTextGrayColor = const Color(0xFFD1D5D6);
-  static Color dTextGrayColor = const Color(0xFFD1D5D6);
+  // static Color lDisableColor = const Color(0xFFD1D4D5);
+  // static Color dDisableColor = const Color(0xFFD1D4D5);
 
-  static Color lTextWhiteColor = const Color(0xFFF7F9F9);
-  static Color dTextWhiteColor = const Color(0xFFF7F9F9);
+  // static Color lTextGrayColor = const Color(0xFFD1D5D6);
+  // static Color dTextGrayColor = const Color(0xFFD1D5D6);
 
-  static Color lWhiteColor = const Color(0xFFF8F8F8);
-  static Color dWhiteColor = const Color(0xFFF8F8F8);
+  // static Color lTextWhiteColor = const Color(0xFFF7F9F9);
+  // static Color dTextWhiteColor = const Color(0xFFF7F9F9);
 
-  static const dBlackColor = Color(0xFF222222);
-  static const lBlackColor = Color(0xFF222222);
+  // static Color lWhiteColor = const Color(0xFFF8F8F8);
+  // static Color dWhiteColor = const Color(0xFFF8F8F8);
 
-  static var lNavInActiveColor = const Color(0xFFA0AFCD);
-  static var dNavInActiveColor = const Color(0xFFA0AFCD);
+  // static const dBlackColor = Color(0xFF222222);
+  // static const lBlackColor = Color(0xFF222222);
 
-  static var lTextFieldColor = const Color(0xFFA3AFB1);
-  static var dTextFieldColor = const Color(0xFFA3AFB1);
+  // static var lNavInActiveColor = const Color(0xFFA0AFCD);
+  // static var dNavInActiveColor = const Color(0xFFA0AFCD);
 
-  static var lBgColor = const Color(0xFFF6F6F6);
-  static var dBgColor = const Color(0xFFF6F6F6);
+  // static var lTextFieldColor = const Color(0xFFA3AFB1);
+  // static var dTextFieldColor = const Color(0xFFA3AFB1);
 
-  static var lRememberMeColor = const Color(0xFF747474);
-  static var dRememberMeColor = const Color(0xFF747474);
+  // static var lBgColor = const Color(0xFFF6F6F6);
+  // static var dBgColor = const Color(0xFFF6F6F6);
 
-  static var lMainTextColor = const Color(0xFF0B0B0B);
-  static var dMainTextColor = const Color(0xFFE1E1E1);
+  // static var lRememberMeColor = const Color(0xFF747474);
+  // static var dRememberMeColor = const Color(0xFF747474);
 
-  static var lBorderColor = const Color(0xFFDEDDDD);
-  static var dBorderColor = const Color(0xFFDEDDDD);
+  // static var lMainTextColor = const Color(0xFF0B0B0B);
+  // static var dMainTextColor = const Color(0xFFE1E1E1);
+
+  // static var lBorderColor = const Color(0xFFDEDDDD);
+  // static var dBorderColor = const Color(0xFFDEDDDD);
 }
