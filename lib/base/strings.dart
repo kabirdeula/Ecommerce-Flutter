bc672437311597b7ce5243897c866efff37cf082
Kabir Deula <kabirdeula167@gmail.com>
2023-11-27 06:57:51 +0545

    added strings

--- lib/base/strings.dart
+++ lib/base/strings.dart
@@ -1,3 +1,11 @@
 class Strings {
   static String appTitle = 'Daraz Lite';
+  // Navigation Bar
+  static String navBarHome = 'Home';
+  static String navBarDiscover = 'Discover';
+  static String navBarMessage = 'Messages';
+  static String navBarCart = 'Cart';
+  static String navBarAccount = 'Account';
+  static String messageCount = '33';
+  static String cartCount = '7';
 }
